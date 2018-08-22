c020b29a164ebcf5527bbe27060f32205193c4bc
Anatoly Kapkov <afftarkapkov@mail.ru>
2018-08-22 17:54:30 +0300

    Hello World with empty main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
+    static {
+        System.out.println("Hello World!");
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Empty main
     }
 }
